a0590c69cdc5b37755681e32b589c554c223253d
梁柱 <liangzhu@77ircloud.com>
2019-05-06 14:10:34 +0800

    feat: 仅保留一个alias ~

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -359,8 +359,7 @@ module.exports = function(webpackEnv) {
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-        src: path.resolve(__dirname, '../src'),
-        packages: path.resolve(__dirname, '../src/packages'),
+        "~": path.resolve(__dirname, '../src'),
         'react-native': 'react-native-web',
       },
       plugins: [
